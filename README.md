00eab4a8904ea9281d23d7bf9bcc369221d883dd
Nick Tsai <myintaer@gmail.com>
2017-12-03 10:58:50 +0800

    Release version 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,100 @@
-# phpexcel-helper
 PHPExcel Helper
+===============
+
+Creating Excel with easy and artistic way based on PHPExcel
+
+---
+
+DEMONSTRATION
+-------------
+
+```php
+\PHPExcelHelper::newExcel()
+    ->addRow(['ID', 'Name', 'Email'])
+    ->addRows([
+        ['1', 'Nick','myintaer@gmail.com'],
+        ['2', 'Eric','eric@.....'],
+    ])
+    ->output('My Excel');
+```
+
+---
+
+INSTALLATION
+------------
+
+Run Composer in your Codeigniter project:
+
+    composer require yidas/phpexcel-helper
+    
+---
+
+USAGE
+-----
+
+### Merge Cells
+
+```php
+\PHPExcelHelper::newExcel()
+    ->addRows([
+        [['value'=>'SN', 'row'=>2], ['value'=>'Language', 'col'=>2], ['value'=>'Block', 'row'=>2, 'col'=>2]],
+        ['','English','繁體中文',['skip'=>2]],
+    ])
+    ->addRows([
+        ['1', 'Computer','電腦','#15'],
+        ['2', 'Phone','手機','#4','#62'],
+    ])
+    ->output('Merged Excel');
+```
+
+### PHPExcel & Sheet Object
+
+```php
+$objPHPExcel = new \PHPExcel;
+$objPHPExcel->getProperties()
+    ->setCreator("Nick Tsai")
+    ->setTitle("Office 2007 XLSX Document");
+$objPHPExcelSheet = $objPHPExcel->setActiveSheetIndex(0);
+$objPHPExcelSheet->setTitle('Sheet');
+$objPHPExcelSheet->setCellValue('A1', 'SN');
+\PHPExcelHelper::newExcel($objPHPExcel)
+    ->setSheet($objPHPExcelSheet)
+    ->setRowOffset(1) // Point to 1nd row from 0
+    ->addRows([
+        ['1'],
+        ['2'],
+    ]);
+\PHPExcelHelper::output();
+```
+
+```php
+\PHPExcelHelper::newExcel()
+    ->setSheet(0, 'Sheet')
+    ->addRow(['SN']);
+$objPHPExcel = \PHPExcelHelper::getExcel();
+$objPHPExcel->getProperties()
+    ->setCreator("Nick Tsai")
+    ->setTitle("Office 2007 XLSX Document");
+$objPHPExcelSheet = \PHPExcelHelper::getSheet();
+$objPHPExcelSheet->setCellValue('A2', '1');
+$objPHPExcelSheet->setCellValue('A3', '2');
+\PHPExcelHelper::output();
+```
+
+### Multiple Sheets
+
+```php
+\PHPExcelHelper::newExcel()
+    ->setSheet(3, '4nd Sheet')
+    ->addRow(['ID', 'Name'])
+    ->addRows([
+        ['1', 'Nick'],
+    ]);
+\PHPExcelHelper::setSheet(1, '2nd Sheet')
+    ->addRow(['SN', 'Title'])
+    ->addRows([
+        ['1', 'Foo'],
+    ]);
+\PHPExcelHelper::output('MultiSheets');
+```
+
---
+++ src/PHPExcelHelper.php
@@ -0,0 +1,349 @@
+<?php
+
+/**
+ * PHPExcel Helper
+ * 
+ * @author      Nick Tsai <myintaer@gmail.com>
+ * @version     1.0.0
+ * @filesource 	PHPExcel <https://github.com/PHPOffice/PHPExcel>
+ * @see         https://github.com/yidas/phpexcel-helper
+ * @example
+ *  \PHPExcelHelper::newExcel()
+ *      ->addRow(['ID', 'Name', 'Email'])
+ *      ->addRows([
+ *          ['1', 'Nick','myintaer@gmail.com'],
+ *          ['2', 'Eric','eric@.....'],
+ *      ])
+ *      ->output('My Excel');
+ */
+class PHPExcelHelper
+{
+    /**
+     * @var object Cached PHPExcel object
+     */
+    private static $_objExcel;
+    
+    /**
+     * @var object Cached PHPExcel Sheet object
+     */
+    private static $_objSheet;
+
+    /**
+     * @var int Current row offset for the actived sheet
+     */
+    private static $_offsetRow;
+
+    /**
+     * @var int Current column offset for the actived sheet
+     */
+    private static $_offsetCol;
+
+    /** 
+     * New or set an PHPExcel object
+     * 
+     * @param object $phpexcelObj PHPExcel object
+     * @return self
+     */
+    public static function newExcel($phpexcelObj=NULL)
+    {
+        self::$_objExcel = (is_object($phpexcelObj))
+            ? $phpexcelObj
+            : new \PHPExcel();
+        
+        return new static();
+    }
+
+    /** 
+     * Get PHPExcel object from cache
+     * 
+     * @return object PHPExcel object
+     */
+    public static function getExcel()
+    {
+        return self::$_objExcel;
+    }
+    
+    /** 
+     * Reset cached PHPExcel sheet object and helper data
+     * 
+     * @return self
+     */
+    public static function resetSheet()
+    {
+        self::$_objSheet = NULL;
+        self::$_offsetRow = 0;
+        self::$_offsetCol = 0;
+
+        return new static();
+    }
+
+    /** 
+     * Set an active PHPExcel Sheet
+     * 
+     * @param object|int $sheet PHPExcel sheet object or index number
+     * @param string $title Sheet title
+     * @return self
+     */
+    public static function setSheet($sheet=0, $title=NULL)
+    {
+        self::resetSheet();
+
+        if (is_object($sheet)) {
+            
+            self::$_objSheet = &$sheet;
+        } 
+        elseif (is_numeric($sheet) && $sheet>=0 && self::$_objExcel) {
+
+            /* Sheets Check */
+            $sheetCount = self::$_objExcel->getSheetCount();
+            if ($sheet >= $sheetCount) {
+                for ($i=$sheetCount; $i <= $sheet; $i++) { 
+                    self::$_objExcel->createSheet($i);
+                }
+            }
+            // Select sheet
+            self::$_objSheet = self::$_objExcel->setActiveSheetIndex($sheet);
+        }
+        else {
+            throw new Exception("Invalid or empty PHPExcel Object for setting sheet", 400);
+        }
+
+        // Sheet Title
+        if ($title) {
+            self::$_objSheet->setTitle($title);
+        }
+
+        return new static();
+    }
+
+    /** 
+     * Get PHPExcel Sheet object from cache
+     * 
+     * @return object PHPExcel Sheet object
+     */
+    public static function getSheet()
+    {
+        return self::$_objSheet;
+    }
+
+    /** 
+     * Set the offset of rows for the actived PHPExcel Sheet
+     * 
+     * @param int $var The offset number
+     * @return self
+     */
+    public static function setRowOffset($var=0)
+    {
+        self::$_offsetRow = (int)$var;
+        
+        return new static();
+    }
+
+    /** 
+     * Get the offset of rows for the actived PHPExcel Sheet
+     * 
+     * @return int The offset number
+     */
+    public static function getRowOffset()
+    {
+        return self::$_offsetCol;
+    }
+
+    /** 
+     * Set the offset of columns for the actived PHPExcel Sheet
+     * 
+     * @param int $var The offset number
+     * @return self
+     */
+    public static function setColumnOffset($var=0)
+    {
+        self::$_offsetCol = (int)$var;
+        
+        return new static();
+    }
+
+    /**
+     * Add a row to the actived sheet of PHPExcel
+     * 
+     * @param array $rowData 
+     *  @param mixed|array Cell value | Data set 
+     *   Data set key-value:
+     *   @param int 'col' Column span for mergence
+     *   @param int 'row' Row span for mergence
+     *   @param int 'skip' Column skip counter
+     *   @param mixed 'value' Cell value
+     * @return self
+     */
+    public static function addRow($rowData)
+    {
+        $sheetObj = self::validSheetObj();
+        
+        // Column pointer
+        $posCol = self::$_offsetCol;
+
+        // Next row
+        self::$_offsetRow++;
+        
+        foreach ($rowData as $key => $value) {
+            
+            // Merge Cells option
+            if (is_array($value)) {
+                
+                // Options
+                $colspan = isset($value['col']) ? $value['col'] : 1;
+                $rowspan = isset($value['row']) ? $value['row'] : 1;
+                $skip = isset($value['skip']) ? $value['skip'] : 1;
+                $value = isset($value['value']) ? $value['value'] : NULL;
+
+                $sheetObj->setCellValueByColumnAndRow($posCol, self::$_offsetRow, $value);
+
+                // Merge handler
+                if ($colspan>1 || $rowspan>1) {
+                    $posColLast = $posCol;
+                    $posCol = $posCol + $colspan - 1;
+                    $posRow = self::$_offsetRow + $rowspan - 1;
+                    $mergeVal = self::num2alpha($posColLast).self::$_offsetRow
+                        . ':'
+                        . self::num2alpha($posCol).$posRow;
+                    $sheetObj->mergeCells($mergeVal);
+                }
+                // Skip option
+                $posCol += $skip;
+
+            } else {
+
+                $sheetObj->setCellValueByColumnAndRow($posCol, self::$_offsetRow, $value);
+                
+                $posCol++;
+            }
+        }
+
+        return new static();
+    }
+
+    /**
+     * Add rows to the actived sheet of PHPExcel
+     * 
+     * @param array array of rowData for addRow()
+     * @return self
+     */
+    public static function addRows($data)
+    {
+         foreach ($data as $key => $row) {
+
+            self::addRow($row);
+        }
+
+        return new static();
+    }
+
+    /** 
+     * Output an Excel file
+     * 
+     * @param string $filename
+     * @param string $excelType
+     */
+    public static function output($filename='excel', $excelType='Excel2007')
+    {
+        $objPHPExcel = self::validExcelObj();
+        
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header('Content-Disposition: attachment;filename="'.$filename.'.xlsx"');
+        header('Cache-Control: max-age=0');
+        // If you're serving to IE 9, then the following may be needed
+        header('Cache-Control: max-age=1');
+        // If you're serving to IE over SSL, then the following may be needed
+        header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+        header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
+        header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+        header ('Pragma: public'); // HTTP/1.0
+        $objWriter = \PHPExcel_IOFactory::createWriter($objPHPExcel, $excelType);
+        $objWriter->save('php://output');
+        exit;
+    }
+
+    /**
+     * Number to Alpha
+     * 
+     * @example
+     *  0 => A, 26 => AA
+     * @param int $n column number
+     * @return string Excel column alpha
+     */
+    public static function num2alpha($n)
+    {
+        $r = '';
+        for ($i = 1; $n >= 0 && $i < 10; $i++) {
+            $r = chr(0x41 + ($n % pow(26, $i) / pow(26, $i - 1))) . $r;
+            $n -= pow(26, $i);
+        }
+        return $r;
+    }
+
+    /**
+     * Alpha to Number
+     * 
+     * @example
+     *  A => 0, AA => 26 
+     * @param int $n Excel column alpha
+     * @return string column number
+     */
+    public static function alpha2num($a)
+    {
+        $r = 0;
+        $l = strlen($a);
+        for ($i = 0; $i < $l; $i++) {
+            $r += pow(26, $i) * (ord($a[$l - $i - 1]) - 0x40);
+        }
+        return $r - 1;
+    }
+
+    /**
+     * Validate and return the selected PHPExcel Object
+     * 
+     * @param object $excelObj PHPExcel Object
+     * @return object Cached object or given object
+     */
+    private static function validExcelObj($excelObj=NULL)
+    {
+        if (is_object($excelObj)) {
+
+            return $excelObj;
+        } 
+        elseif (is_object(self::$_objExcel)) {
+
+            return self::$_objExcel;
+        } 
+        else {
+            
+            throw new Exception("Invalid or empty PHPExcel Object", 400);
+        }
+    }
+
+    /**
+     * Validate and return the selected PHPExcel Sheet Object
+     * 
+     * @param object $excelObj PHPExcel Sheet Object
+     * @return object Cached object or given object
+     */
+    private static function validSheetObj($sheetObj=NULL)
+    {
+        if (is_object($sheetObj)) {
+
+            return $sheetObj;
+        } 
+        elseif (is_object(self::$_objSheet)) {
+
+            return self::$_objSheet;
+        } 
+        elseif (is_object(self::$_objExcel)) {
+
+            // Set to default sheet if is unset
+            return self::setSheet()->getSheet();
+        }
+        else {
+            
+            throw new Exception("Invalid or empty PHPExcel Sheet Object", 400);
+        }
+    }
+}
